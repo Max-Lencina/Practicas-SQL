3ef5a0062a8f8f0f87783cb5e3bc14de03de8894
Max <109391388+Max-Lencina@users.noreply.github.com>
2022-10-05 16:46:40 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,1 +1,5 @@
-SELECT * FROM portfolio.experiencia;
+INSERT INTO persona VALUES (1, 'Maximiliano', 'Lencina', 'assets/img/perfil.jpg', 'assets/img/portada.jpg', 'Full Stack Developer Junior', 'Soy de Paraná, Entre Ríos, Argentina.', 'maxilencina1985@gmail.com')
+
+INSERT INTO experiencia VALUES (1, 'La Argentina', 0, '2004-06-08', '2009-08-05', 'Cajero', 1 )	
+
+SELECT * FROM portfolio.persona;
